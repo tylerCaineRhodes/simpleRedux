d15fe1195a790a0077d14e4c0b7eb0205bf3fea0
Tyler Rhodes <rhodetyl000@gmail.com>
2020-11-02 11:17:36 -0600

    Add dev tools comment

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import Counter from './components/Counter';
 import * as serviceWorker from './serviceWorker';
 
+//add dev tools
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 ReactDOM.render(
     <Counter />,
   document.getElementById('root')
 );
-
 serviceWorker.unregister();
